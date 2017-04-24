291276da66bc640245a4b89a6917cd148a4fc360
2ndkauboy <bernhard@kau-boys.de>
2017-04-24 18:13:06 +0200

    Added the page template with no sidebar and full width

---
+++ templates/page-full-width.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Template Name: Full Width
+ *
+ * @package CampSite_2017
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area full-width">
+		<main id="main" class="site-main" role="main">
+
+			<?php
+			while ( have_posts() ) : the_post();
+
+				get_template_part( 'template-parts/content', 'page' );
+
+				// If comments are open or we have at least one comment, load up the comment template.
+				if ( comments_open() || get_comments_number() ) :
+					comments_template();
+				endif;
+
+			endwhile; // End of the loop.
+			?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php
+get_footer();
---
+++ templates/page-no-sidebar.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Template Name: No Sidebar
+ *
+ * @package CampSite_2017
+ */
+
+
+get_header(); ?>
+
+	<div id="primary" class="content-area no-sidebar">
+		<main id="main" class="site-main" role="main">
+
+			<?php
+			while ( have_posts() ) : the_post();
+
+				get_template_part( 'template-parts/content', 'page' );
+
+				// If comments are open or we have at least one comment, load up the comment template.
+				if ( comments_open() || get_comments_number() ) :
+					comments_template();
+				endif;
+
+			endwhile; // End of the loop.
+			?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php
+get_footer();
