3091cec9073320812c6720e0c578164750c31d33
2ndkauboy <bernhard@kau-boys.de>
2017-05-25 18:29:10 +0200

    Fixed pluralization for languags with the singular form on other numbers as one (such as Russian). See #4.

--- comments.php
+++ comments.php
@@ -27,12 +27,19 @@ if ( post_password_required() ) {
 	if ( have_comments() ) : ?>
 		<h2 class="comments-title">
 			<?php
+			$comments_number = get_comments_number();
+
+			if ( '1' === $comments_number ) {
+				/* translators: %s: post title */
+				printf( _x( 'One Reply to &ldquo;%s&rdquo;', 'comments title', 'twentyseventeen' ), get_the_title() ); // WPCS: XSS OK.
+			} else {
 				printf( // WPCS: XSS OK.
-					/* translators: 1: comments count, 2: comment title */
-					esc_html( _nx( 'One thought on &ldquo;%2$s&rdquo;', '%1$s thoughts on &ldquo;%2$s&rdquo;', get_comments_number(), 'comments title', 'wordcamporg' ) ),
-					number_format_i18n( get_comments_number() ),
-					'<span>' . get_the_title() . '</span>'
+					/* translators: 1: number of comments, 2: post title */
+					_nx( '%1$s Reply to &ldquo;%2$s&rdquo;', '%1$s Replies to &ldquo;%2$s&rdquo;', $comments_number, 'comments title', 'wordcamporg' ),
+					number_format_i18n( $comments_number ),
+					get_the_title()
 				);
+			}
 			?>
 		</h2><!-- .comments-title -->
 
